d5cc39c4561fc6a5f64700b56ea9fc0b0357d1b7
Rachel Mathew <rachelmathew999@gmail.com>
2014-02-17 22:12:43 -0500

    Completed find_track_regex function

--- hw02/find_track.c
+++ hw02/find_track.c
@@ -8,6 +8,7 @@ License: Creative Commons Attribution-ShareAlike 3.0
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <regex.h>
 
 #define NUM_TRACKS 5
 
@@ -27,9 +28,9 @@ void find_track(char search_for[])
 {
     int i;
     for (i=0; i<NUM_TRACKS; i++) {
-	if (strstr(tracks[i], search_for)) {
-	    printf("Track %i: '%s'\n", i, tracks[i]);
-	}
+    if (strstr(tracks[i], search_for)) {
+        printf("Track %i: '%s'\n", i, tracks[i]);
+    }
     }
 }
 
@@ -38,7 +39,26 @@ void find_track(char search_for[])
 // Prints track number and title.
 void find_track_regex(char pattern[])
 {
-    // TODO: fill this in
+     
+    int status; 
+    regex_t re; 
+
+    //Regcomp compiles a regular expression, prints an error
+    // message if nessacary, compilation stored in re
+    if(regcomp(&re,pattern, REG_EXTENDED|REG_NOSUB) != 0){
+        printf("An error in regcomp");
+        return;
+    }
+    int i;
+    for(i = 0; i < NUM_TRACKS; i++){
+        //regexec matches tracks against the compiled regular exp.
+        status = regexec(&re, tracks[i], (size_t) 0, NULL, 0);
+        if (status == 0) {
+            printf("Track %i: '%s'\n",i,tracks[i]);
+        }
+    }
+    regfree(&re);
+    return;    
 }
 
 // Truncates the string at the first newline, if there is one.
@@ -46,7 +66,7 @@ void rstrip(char s[])
 {
     char *ptr = strchr(s, '\n');
     if (ptr) {
-	*ptr = '\0';
+    *ptr = '\0';
     }
 }
 
@@ -59,8 +79,8 @@ int main (int argc, char *argv[])
     fgets(search_for, 80, stdin);
     rstrip(search_for);
 
-    find_track(search_for);
-    //find_track_regex(search_for);
+    // find_track(search_for);
+    find_track_regex(search_for);
 
     return 0;
 }
